7ee92429886db0ad74c164c8c8509c8ffa614529
Anik <66571431+NavinAnik@users.noreply.github.com>
2023-06-02 21:38:59 +0600

    Added my Analog Clock animation

--- contributors.js
+++ contributors.js
@@ -9,6 +9,16 @@ const contributors = [
 		linkedinProfile: 'https://www.linkedin.com/in/adnansabbir/',
 		githubProfile: 'https://github.com/adnansabbir',
 	},
+	{
+		name: 'Analog clock',
+		description: 'A simple analog clock animation for your homepage',
+		previewImageLink: 'https://res.cloudinary.com/dtohlforn/image/upload/v1685720081/ezgif-2-623689cb51_lo87t1.gif',
+		author: 'S. M. Navin Nayer Anik',
+		folderName: 'navinanik',
+		facebookProfile: 'https://www.facebook.com/smnavinnayeranik',
+		linkedinProfile: 'https://www.linkedin.com/in/s-m-navin-nayer-anik-8011441b1/',
+		githubProfile: 'https://github.com/NavinAnik',
+	},
 	// Add your contributor info above this line and don't remove these line. 
 	// Make sure you put a comma after your second bracket.
 ]
